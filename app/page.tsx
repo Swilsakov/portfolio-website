d0e123400727b22f8578a4e030174eb7b4774294
Vladimir <shkodaonebob@gmail.com>
2024-10-25 00:31:56 +0600

    add clients and experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
+        <Experience />
       </div>
     </main>
   );
