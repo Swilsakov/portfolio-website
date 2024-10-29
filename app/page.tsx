affb4de8e04e8b2ce2f52e04418b88c4dceaef2d
Vladimir <shkodaonebob@gmail.com>
2024-10-29 16:12:07 +0600

    some updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
-import Clients from "@/components/Clients";
+// import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
-import Approach from "@/components/Approach";
+// import Approach from "@/components/Approach";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -16,9 +16,9 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients />
+        {/* <Clients /> */}
         <Experience />
-        <Approach />
+        {/* <Approach /> */}
         <Footer />
       </div>
     </main>
